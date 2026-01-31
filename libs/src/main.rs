d336aebbf474be815ba6b3b6839f812cd57d754c
pivoteur <dauclair@hotmail.com>
2026-01-31 00:57:32 -0500

    transitioned to using book::test_utils

--- libs/src/main.rs
+++ libs/src/main.rs
@@ -2,8 +2,8 @@
 
 use book::{
    err_utils::ErrStr,
-   string_utils::{plural,words},
-   utils::pred
+   string_utils::words,
+   test_utils::collate_results
 };
 
 use libs::{
@@ -11,31 +11,16 @@ use libs::{
    tables::functional_tests::runoff as t
 };
 
-fn tests() -> Vec<String> {
-   words("git tables")
-   // vec!["git", "tables"].into_iter().map(to_string).collect()
+fn test_names() -> Vec<String> { words("git table") }
+async fn tests() -> Vec<ErrStr<()>> {
+   // let names = test_names();
+   let res = vec![g().await, t()];
+   // names.into_iter().zip(res.into_iter()).collect()
+   res
 }
 
 #[tokio::main]
 async fn main() -> ErrStr<()> {
-   let res = [g().await];
-   let len = res.len();
-   if res.iter().all(Result::is_ok) {
-      println!("\nAll {} passed.", plural(len, "functional test"));
-      Ok(())
-   } else {
-      failures(&res, len)
-   }
-}
-
-fn failures(res: &[ErrStr<()>], len: usize) -> ErrStr<()> {
-   let fs: Vec<String> =
-      res.iter()
-         .enumerate()
-         .filter_map(|(n,r)| pred(!r.is_ok(), tests()[n].clone()))
-         .collect();
-   let many = plural(fs.len(), &format!("/{len} functional test"));
-   println!("The following {} FAILED!:\n\t{}", many, fs.join("\n\t"));
-   Err(format!("{} FAILED!", many))
+   collate_results(&tests().await, &test_names())
 }
 
--- quizzes/src/main.rs
+++ quizzes/src/main.rs
@@ -1,6 +1,6 @@
 use book::{
    err_utils::ErrStr,
-   string_utils::plural
+   test_utils::collate_results
 };
 
 use quizzes::{
@@ -9,25 +9,14 @@ use quizzes::{
    quiz09::functional_tests::runoff as i
 };
 
+fn test_names() -> Vec<String> {
+   [1,2,9].iter().map(|n| format!("quiz0{n}")).collect()
+}
+async fn tests() -> Vec<ErrStr<()>> { [a().await, b().await, i()].to_vec() }
+
 #[tokio::main]
 async fn main() -> ErrStr<()> {
    println!("\nquizzes functional tests\n");
-   fn okey() -> ErrStr<()> { Ok(()) }
-   let (c,d,e,f,g,h) = (okey(), okey(), okey(), okey(), okey(), okey());
-   let test_results = [a().await, b().await, c, d, e, f, g, h, i()];
-   let res: Vec<usize> = 
-      test_results.iter()
-                  .enumerate()
-                  .filter_map(|(a,b)| if b.is_ok() { None } else { Some(a+1) })
-                  .collect();
-   let n = test_results.len();
-   if res.is_empty() {
-      let tests = plural(n, "functional test");
-      println!("\nAll {tests} passed.");
-      Ok(())
-   } else {
-      let fails = plural(res.len(), &format!("/{n} functional test"));
-      Err(format!("{fails} failed for quizzes {res:?}"))
-   }
+   collate_results(&tests().await, &test_names())
 }
 
