037cef9b6154c4ee04815b29ba6c8bce5f2f897a
pivoteur <dauclair@hotmail.com>
2026-03-09 12:30:03 -0400

    [WIP] reading and writing open-pivot pools

--- libs/src/tables.rs
+++ libs/src/tables.rs
@@ -1,14 +1,17 @@
 use book::{
+   csv_utils::CsvHeader,
    err_utils::ErrStr,
-   list_utils::ht,
+   list_utils::{ht,tail},
    parse_utils::{parse_id,parse_str},
    table_utils::{Table,ingest}
 };
 
 use super::types::util::Id;
 
+pub type IxTable = Table<Id, String, String>;
+
 /// From a set of row-data, index the rows and parse into a table
-pub fn index_table(lines: Vec<String>) -> ErrStr<Table<Id, String, String>> {
+pub fn index_table(lines: Vec<String>) -> ErrStr<IxTable> {
    let (h, t) = ht(&lines);
    let h1 = h.ok_or("empty list for data set")?;
    let header = format!("ix\t{h1}");
@@ -16,6 +19,16 @@ pub fn index_table(lines: Vec<String>) -> ErrStr<Table<Id, String, String>> {
       t.iter().enumerate().map(|(a, b)| format!("{a}\t{b}")).collect();
    body.insert(0, header);
    ingest(parse_id, parse_str, parse_str, &body, "\t")
+}
+
+pub fn print_sans_index(t: &IxTable) {
+   let hdrs: Vec<String> = tail(&t.header().split(",").collect());
+   let hdr = hdrs.join("\t");
+   println!("{hdr}");
+   for row in t.data {
+      let r = row.join("\t");
+      println!("{r}");
+   } // by printing the rows of data, we don't print the index (the ROW Id)
 }
 
 pub mod functional_tests {
@@ -35,10 +48,13 @@ pub mod functional_tests {
    }
 
    pub fn runoff() -> ErrStr<usize> {
-      println!("\ntables functional test\n");
+      println!("\ntables functional tests\n");
       let table = index_table(some_rows())?;
       println!("Indexed table is:\n\n{}", table.as_csv());
-      Ok(1)
+
+      println!("\nTable without indices as TSV:\n");
+      print_sans_index(&table);
+      Ok(2)
    }
 }
 
--- quizzes/src/quiz11/a_rwr/mod.rs
+++ quizzes/src/quiz11/a_rwr/mod.rs
@@ -1,4 +1,9 @@
-use book::{ csv_utils::print_as_tsv, err_utils::ErrStr };
+use book::{
+   csv_utils::print_as_tsv,
+   err_utils::ErrStr,
+   file_utils::lines_from_file
+};
 
 use libs::fetchers::fetch_pivots;
+
 
