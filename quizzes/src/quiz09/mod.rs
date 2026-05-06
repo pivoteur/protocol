c768740ae63fda8bf7bb557d119d1a1774457cbb
pivoteur <dauclair@hotmail.com>
2026-05-06 13:29:52 -0400

    quiz09 functional tests updated

--- quizzes/src/quiz09/mod.rs
+++ quizzes/src/quiz09/mod.rs
@@ -1,14 +1,2 @@
-
 pub mod a_itr;
 
-#[cfg(not(tarpaulin_include))]
-pub mod functional_tests {
-   use super::a_itr::functional_tests::runoff as a;
-
-   use book::err_utils::ErrStr;
-
-   pub fn runoff() -> ErrStr<usize> {
-      println!("\nquiz09 functional tests\n");
-      a()
-   }
-}
