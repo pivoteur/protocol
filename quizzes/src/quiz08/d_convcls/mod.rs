79be48251fad48cebf7c291b7fcf973274786e62
pivoteur <dauclair@hotmail.com>
2026-08-12 08:14:12 -0400

    [wip] to populate opened-field for the close pivots

--- quizzes/src/quiz08/d_convcls/mod.rs
+++ quizzes/src/quiz08/d_convcls/mod.rs
@@ -21,7 +21,7 @@ use libs::{
 /// convcls computes the 10% gains from the open pivot table.
 #[derive(Debug, Parser)]
 #[command(name = "convcls")]
-#[command(version = "1.03")]
+#[command(version = "1.04")]
 struct Args {
    /// Path to the open pivots table
    opens: String,
